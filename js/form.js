93ebeb4471fad6df9c4101d61634693798ea6f76
Safe_Mode <safe2mode@gmail.com>
2018-04-16 12:49:49 +0300

    Исправляет активацию формы

--- js/form.js
+++ js/form.js
@@ -5,32 +5,23 @@
   var formFieldsEl = formEl.querySelectorAll('fieldset');
   var mainPinEl = document.querySelector('.map__pin--main');
 
-  var toggleFieldsState = function () {
-    if (formEl.classList.contains('ad-form--disabled')) {
-      formFieldsEl.forEach(function (field) {
-        field.disabled = true;
-      });
-    } else {
-      formFieldsEl.forEach(function (field) {
-        field.disabled = false;
-      });
-    }
-  };
-
-  var toggleFormState = function () {
+  var activateForm = function () {
     if (formEl.classList.contains('ad-form--disabled')) {
       formEl.classList.remove('ad-form--disabled');
-    } else {
-      formEl.classList.add('ad-form--disabled');
     }
 
-    toggleFieldsState();
+    formFieldsEl.forEach(function (field) {
+      field.disabled = false;
+    });
   };
 
   var onMainPinMouseUp = function () {
-    toggleFormState();
+    activateForm();
   };
 
-  toggleFieldsState();
+  formFieldsEl.forEach(function (field) {
+    field.disabled = true;
+  });
+
   mainPinEl.addEventListener('mouseup', onMainPinMouseUp);
 })();
